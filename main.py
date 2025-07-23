20da47380ac906e801cb7ceb7a8bc71b8c0f94c2
Corina <corinatft@gmail.com>
2025-07-23 21:59:25 +0300

    Add extra API routes to main.py

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
+class Event(BaseModel):
+    title: str
+    date: str
+    time: str
+
+events = []
+
+@app.post("/events")
+def add_event(event: Event):
+    events.append(event)
+    return {"message": "Event added", "event": event}
+
+@app.get("/events")
+def get_events():
+    return events
+
 
 # Lancer le serveur avec la commande suivante :
 # uvicorn main:app --reload
